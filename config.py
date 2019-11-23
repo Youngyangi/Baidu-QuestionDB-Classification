fa635e4e9ca227a9fc4bc3bd2a84e20d3c8128a5
Youngyang <Youngyang@whu.edu.cn>
2019-11-24 00:14:02 +0800

    init

---
+++ config.py
@@ -0,0 +1,12 @@
+import os.path
+import pathlib
+
+pwd_path = pathlib.Path(os.path.abspath(__file__)).parent
+
+source_dir = os.path.join(pwd_path, 'Data/百度题库')
+
+history_source_dir = os.path.join(source_dir, '高中_历史/origin')
+geo_source_dir = os.path.join(source_dir, '高中_地理/origin')
+poli_source_dir = os.path.join(source_dir, '高中_政治/origin')
+bio_source_dir = os.path.join(source_dir, '高中_生物/origin')
+
